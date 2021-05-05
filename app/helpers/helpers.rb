6933b328ba64d87f39b468618dc1152c0248672c
MuhammadDanyalMahdi <danyalm50@gmail.com>
2021-05-05 13:51:57 -0400

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,13 @@
 class Helpers
+
+    def current_user(session)
+        @user = User.find_by_id(session[:user.id])
+        @user
+    end
+
+    def is_logged_in?(session)
+        !!session[:user.id]
+    end
+            
+
 end
